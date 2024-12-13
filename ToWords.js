7a66f22eba214364f9f3cddf2287469339385ecb
smsguy927 <smsguy927@gmail.com>
2024-12-12 21:51:27 -0600

    Number to words correct up to Number.MAX_SAFE_INTEGER

--- ToWords.js
+++ ToWords.js
@@ -37,11 +37,13 @@ const tens = [
 Object.freeze(tens);
 
 const ions = [
+    null,
     "thousand",
     "million",
     "billion",
     "trillion",
     "quadrillion",
+    // not used until refactored to allow strings and BigInt
     "quintillion",
     "sextillion",
     "septillion",
@@ -53,22 +55,32 @@ Object.freeze(ions);
 
 const toWords = num => {
     const words = [];
+    let ionsIt = 0
     while (num > 0) {
-
-        if (num < 20) {
-            words.push(under20[num]);
+        ionsIt && words.push(ions[ionsIt]);
+        const tensOnes = num % 100
+        if (tensOnes < 20) {
+            words.push(under20[tensOnes]);
             num = Math.floor(num / 100)
         } else {
             const ones = num % 10
-            if (ones) {
-                words.push(under20[ones])
-            }
+            ones && words.push(under20[ones])
+
             num = Math.floor(num / 10)
             words.push(tens[num % 10])
             num = Math.floor(num / 10)
         }
-        if(num > 0) {
-            words.push('hundred');
+        if (num > 0) {
+            const ones = num % 10
+            if (ones) {
+                words.push('hundred');
+                words.push(under20[ones]);
+            }
+
+            num = Math.floor(num / 10)
+        }
+        if (num > 0) {
+            ionsIt++
         }
     }
     return words.reverse().join(' ')
--- test/test.js
+++ test/test.js
@@ -8,10 +8,21 @@ describe('Array', function () {
     });
 });
 describe('toWords()', function () {
-    it('should return a string of words that represent the number passed in', function () {
+    it('should return a string of words that represent the number passed in 0', function () {
         // Setup
         const input = 7
         const expected = "seven"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it('should return a string of words that represent the number passed in 1', function () {
+        // Setup
+        const input = 11
+        const expected = "eleven"
 
         // Exercise
         const result = toWords(input);
@@ -41,7 +52,7 @@ describe('toWords()', function () {
         // Verify
         assert.strictEqual(result, expected);
     })
-    it("doesn't use the null values in the number parts arrays again", function () {
+    it("doesn't use the null values in the number parts arrays again 0", function () {
         // Setup
         const input = 420
         const expected = "four hundred twenty"
@@ -52,10 +63,76 @@ describe('toWords()', function () {
         // Verify
         assert.strictEqual(result, expected);
     })
-    it("converts big numbers correctly", function () {
+    it("doesn't use the null values in the number parts arrays again 1", function () {
+        // Setup
+        const input =  1515
+        const expected = "one thousand five hundred fifteen"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it("converts big numbers correctly 0", function () {
         // Setup
         const input = 80085
         const expected = "eighty thousand eighty five"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it("converts big numbers correctly 1", function () {
+        // Setup
+        const input = 12345
+        const expected = "twelve thousand three hundred forty five"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it("converts big numbers correctly 2", function () {
+        // Setup
+        const input = 666666
+        const expected = "six hundred sixty six thousand six hundred sixty six"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it("converts big numbers correctly 3", function () {
+        // Setup
+        const input = 113650390
+        const expected = "one hundred thirteen million six hundred fifty thousand three hundred ninety"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it("converts big numbers correctly 4", function () {
+        // Setup
+        const input = 109110111
+        const expected = "one hundred nine million one hundred ten thousand one hundred eleven"
+
+        // Exercise
+        const result = toWords(input);
+
+        // Verify
+        assert.strictEqual(result, expected);
+    })
+    it("converts the max safe int in JS", function () {
+        // Setup
+        const input = Number.MAX_SAFE_INTEGER
+        const expected = "nine quadrillion seven trillion one hundred ninety nine billion two hundred fifty four million seven hundred forty thousand nine hundred ninety one"
 
         // Exercise
         const result = toWords(input);
